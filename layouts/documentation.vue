f28b43f8b7d13341a7ddcbf88a515567505966c8
rwampler <rwampler86@msn.com>
2018-10-02 17:00:58 -0700

    fix text selection on documentation pages
    remove accidental css that disabled text selection on documentation
    pages

--- layouts/documentation.vue
+++ layouts/documentation.vue
@@ -37,10 +37,4 @@ body
   font-size: 12px
   margin: 0
   padding: 0
-  -webkit-touch-callout: none
-  -webkit-user-select: none
-  -khtml-user-select: none
-  -moz-user-select: none
-  -ms-user-select: none
-  user-select: none
 </style>
